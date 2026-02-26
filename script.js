8b37699b4438d26690c530e45866eb774fce4582
Harsh Kumar Singh <harshsingh172725@gmail.com>
2026-02-26 16:16:04 +0530

    Create script.js
    To Perform Specific Operation  like (Addition, Multiplication, Division, subtraction , etc) I have Written a JavaScript

---
+++ script.js
@@ -0,0 +1,22 @@
+function appendValue(val){
+  document.getElementById("display").value += val;
+}
+
+function clearDisplay(){
+  document.getElementById("display").value = "";
+}
+
+function deleteLast(){
+  let current = document.getElementById("display").value;
+  document.getElementById("display").value = current.slice(0,-1);
+}
+
+function calculate(){
+  try{
+    document.getElementById("display").value =
+    eval(document.getElementById("display").value);
+  }
+  catch{
+    alert("Invalid Input");
+  }
+}
